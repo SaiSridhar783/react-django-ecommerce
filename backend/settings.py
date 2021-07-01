506cc51c1a18d93bdc8fea4d36be0d8490d8ee75
SaiSridhar783 <sridhar.akula001@gmail.com>
2021-07-01 13:07:09 +0530

    Image Field

--- backend/settings.py
+++ backend/settings.py
@@ -125,6 +125,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+MEDIA_ROOT = BASE_DIR / "static/images"
+MEDIA_URL = "/images/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
