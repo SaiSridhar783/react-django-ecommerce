8e99ec68eecfcdc01618b4587fd43dc701e21ee3
SaiSridhar783 <sridhar.akula001@gmail.com>
2021-07-02 14:34:02 +0530

    Authentication

--- base/views/product_views.py
+++ base/views/product_views.py
@@ -1,32 +1,22 @@
 from django.shortcuts import render
-#from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
-from .models import Product
-from .serializers import ProductSerializer
+from base.models import Product
+from base.serializers import ProductSerializer
 
-# Create your views here.
-
-
-@api_view(["GET"])
-def getRoutes(request):
-    routes = [
-        "/api/",
-        "/api/products/",
-        "/api/product/<id>"
-    ]
-    return Response(routes)
+from rest_framework import status
 
 
-@api_view(["GET"])
+@ api_view(["GET"])
 def getProducts(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
 
-@api_view(["GET"])
+@ api_view(["GET"])
 def getProduct(request, pk):
     product = Product.objects.get(pk=pk)
     serializer = ProductSerializer(product, many=False)
