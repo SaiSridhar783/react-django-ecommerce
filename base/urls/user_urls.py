3ef328fc715f269689376eb43fc6375ca7d4097c
SaiSridhar783 <sridhar.akula001@gmail.com>
2021-07-03 19:02:18 +0530

    Update profile API

--- base/urls/user_urls.py
+++ base/urls/user_urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from base.views import user_views as views
 
 urlpatterns = [
-    path('login', views.MyTokenObtainPairView.as_view(),
+    path('login/', views.MyTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
-    path("register", views.registerUser, name="register"),
+    path("register/", views.registerUser, name="register"),
+    path("profile/update/", views.updateUserProfile, name="user-profile-update"),
     path("profile/", views.getUserProfile, name="user-profile"),
     path("", views.getUsers,  name="users"),
 ]
