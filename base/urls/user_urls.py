431959272a73f51d5798fc0860b9bad506672dee
SaiSridhar783 <sridhar.akula001@gmail.com>
2021-07-05 20:56:32 +0530

    Update User

--- base/urls/user_urls.py
+++ base/urls/user_urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path("profile/update/", views.updateUserProfile, name="user-profile-update"),
     path("profile/", views.getUserProfile, name="user-profile"),
     path("", views.getUsers,  name="users"),
-    path("delete/<str:pk>/", views.deleteUser, name="user-delete")
+    path("<str:pk>/", views.getUserById, name="user-get"),
+    path("update/<str:pk>/", views.updateUser, name="user-update"),
+    path("delete/<str:pk>/", views.deleteUser, name="user-delete"),
 ]
