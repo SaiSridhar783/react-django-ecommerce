e97fff92941f2a4022531569dd71d50ef5a78299
SaiSridhar783 <sridhar.akula001@gmail.com>
2021-07-07 00:01:44 +0530

    changes

--- backend/settings.py
+++ backend/settings.py
@@ -195,6 +195,8 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "http://saisridhar783.pythonanywhere.com/"
+    "https://saisridhar783.pythonanywhere.com/"
 ]
 
 
