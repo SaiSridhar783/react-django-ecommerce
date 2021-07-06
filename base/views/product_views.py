7010e7e5db2c190b104bcaecbbb72825ee272308
SaiSridhar783 <sridhar.akula001@gmail.com>
2021-07-06 12:07:28 +0530

    Default Image

--- base/views/product_views.py
+++ base/views/product_views.py
@@ -9,16 +9,44 @@ from base.serializers import ProductSerializer
 from rest_framework import status
 
 
-@ api_view(["GET"])
+@api_view(["GET"])
 def getProducts(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
 
-@ api_view(["GET"])
+@api_view(["GET"])
 def getProduct(request, pk):
     product = Product.objects.get(pk=pk)
     serializer = ProductSerializer(product, many=False)
 
     return Response(serializer.data)
+
+
+@api_view(["POST"])
+@permission_classes([IsAdminUser])
+def createProduct(request, pk):
+    user = request.user
+    product = Product.objects.create(
+        user=user,
+        name="Sample Name",
+        price=0,
+        brand="Sample Brand",
+        countInStock=0,
+        category="Sample Category",
+        description=""
+    )
+
+    serializer = ProductSerializer(product, many=False)
+
+    return Response(serializer.data)
+
+
+@api_view(["DELETE"])
+@permission_classes([IsAdminUser])
+def deleteProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+    product.delete()
+
+    return Response("Product Deleted Successfully!")
